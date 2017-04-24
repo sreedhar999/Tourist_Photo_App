73e1755685b0142a26ecefef74cdf45db96c4072
sreeharraorv@hotmail.com <sreedharraorv@hotmail.com>
2017-04-23 22:29:01 -0400

    added main page with construction

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
-  
-  scope :api, defaults: {format: :json}  do
+ scope :api, defaults: {format: :json}  do
     resources :foos, except: [:new, :edit]
     resources :bars, except: [:new, :edit]
     end
+    get '/ui' => 'ui#index'
+    get '/ui#' => 'ui#index'
+    root "ui#index"
 end
